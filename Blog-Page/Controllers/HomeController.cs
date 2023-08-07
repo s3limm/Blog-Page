7f95fe0c6db728a50180c709472fd63d1d9c7d91
Selim <selimemrem06@gmail.com>
2023-08-07 12:31:07 +0300

    Initial commit

---
+++ Blog-Page/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Blog_Page.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Blog_Page.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Blog-Page/wwwroot/Website/blog/js/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["/","/about","/blog","/blog/[post_slug]","/blog/tag/[tag_slug]"]),self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB();
