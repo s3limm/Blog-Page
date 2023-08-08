c86c0424e9889659ece5c5ce870a12d846c25cba
sn3al <108307150+s3limm@users.noreply.github.com>
2023-08-08 18:51:21 +0300

    login page modified

--- Blog-Page/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Blog_Page.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
