c85a82c8de9e4f149b8d04950e5ddfdaf18360a4
sn3al <108307150+s3limm@users.noreply.github.com>
2023-08-08 16:43:17 +0300

    added login page

---
+++ Blog-Page/Controllers/BlogAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_Page.Controllers
+{
+    public class BlogAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Blog-Page/Controllers/LogInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_Page.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
