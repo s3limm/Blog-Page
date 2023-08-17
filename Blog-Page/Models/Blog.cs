505187e1c9e60e0f6bea3ef5a2be719f4b5f59a5
Selim <selimemrem06@gmail.com>
2023-08-17 20:33:39 +0300

    added blog entity

---
+++ Blog-Page/Models/Blog.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog_Page.Models
+{
+    public class Blog:BaseEntity
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+    }
+}
