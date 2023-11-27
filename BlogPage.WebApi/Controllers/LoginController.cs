f4f76488d02a28fbf3410af16635c2e4a7f4cab3
Selim <selimemrem06@gmail.com>
2023-11-27 15:26:23 +0300

    Added Blog Entity, Controller and Api Porject

---
+++ BlogPage.WebApi/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPage.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return Created("",new JwtToken.JwtGenerateToken().GenerateToken());
+        }
+    }
+}
---
+++ BlogPage.WebApi/Core/Domain/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace BlogPage.Api.Core.Domain
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            Status = Status.Inserted;
+            CreatedDate = DateTime.Now;
+        }
+
+        //public int ID { get; set; }
+        public Status Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
